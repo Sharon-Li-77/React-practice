77aea1d67573e60152b01e156e6dd0e59a41dbc9
Jared Pinfold <j.pinfold@gmail.com>
2023-10-30 14:48:39 +1300

    styled css to make a better button situation

--- client/components/NeglectThePumpkins.tsx
+++ client/components/NeglectThePumpkins.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 const allPumpkins = ['first', 'second', 'third'] as const
 type Pumpkin = typeof allPumpkins[number]
 const decay = [
@@ -10,49 +8,15 @@ const decay = [
 ]
 
 function NeglectThePumpkins() {
-  // Create a state for the changing img props÷
-  const [pumpk, setPumpk] = useState({
-    // a pumpk with an img prop that starts at 0
-    first: 0,
-    second: 0,
-    third: 0,
-  })
-  //  Version 1 - longer with if else:
-  function handleClick(pumpkin: Pumpkin) {
-    if (pumpk[pumpkin] === 3) {
-      setPumpk({
-        ...pumpk,
-        [pumpkin]: 0,
-      })
-    } else {
-      setPumpk({
-        ...pumpk,
-        [pumpkin]: pumpk[pumpkin] + 1,
-      })
-    }
-  }
-
-  // Version 2 - shorter with ternary babyyyyy:
-  // function handleClick(pumpkin: Pumpkin) {
-  //   setPumpk({
-  //     ...pumpk,
-  //     [pumpkin]: pumpk[pumpkin] === 3 ? 0 : pumpk[pumpkin] + 1,
-  //   })
-  // }
-
   return (
     <>
       {allPumpkins.map((pumpkin) => (
-        <button
-          key={pumpkin}
-          onClick={() => handleClick(pumpkin)}
-          className="p-button"
-        >
+        <button key={pumpkin} className="p-button">
           <img
             id={pumpkin}
             className="pumpkin"
-            src={`client/images/pumpkin-${pumpk[pumpkin]}.png`}
-            alt={decay[pumpk[pumpkin]]} // this should be the index of the image which is changing.
+            src={`client/images/pumpkin-0.png`}
+            alt=""
           />
         </button>
       ))}
